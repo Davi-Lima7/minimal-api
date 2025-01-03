c554f11fea163c14b9bf81fcb617d3205121e9d8
Davi <davilimadealmeida60@gmail.com>
2025-01-03 11:01:49 -0300

    Adicionado JWT e regras de autorizacao

--- Dominio/Interfaces/IAdministradorServico.cs
+++ Dominio/Interfaces/IAdministradorServico.cs
@@ -10,5 +10,8 @@ namespace minimal_api.Dominio.Interfaces
   public interface IAdministradorServico
   {
     Administrador? Login(LoginDTO loginDTO);
+    Administrador Incluir(Administrador administrador);
+    Administrador? BuscarPorId(int id);
+    List<Administrador> Todos(int? pagina);
   }
 }
